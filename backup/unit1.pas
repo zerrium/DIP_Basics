1eebf9d22b1c66631d4647d2d22f4aad2c4faee2
willysusilo <58355441+willysusilo@users.noreply.github.com>
2020-09-30 14:50:51 +0700

    Added button actions for load and save image
    Only can load .bmp files but can save it into .png .jpg or .bmp

--- backup/unit1.pas
+++ backup/unit1.pas
@@ -5,13 +5,20 @@ unit Unit1;
 interface
 
 uses
-  Classes, SysUtils, Forms, Controls, Graphics, Dialogs;
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, ExtCtrls, StdCtrls,
+  ExtDlgs, windows;
 
 type
 
   { TForm1 }
 
   TForm1 = class(TForm)
+    loadButton: TButton;
+    OpenPictureDialog1: TOpenPictureDialog;
+    saveButton: TButton;
+    Image1: TImage;
+    SavePictureDialog1: TSavePictureDialog;
+    procedure loadButtonClick(Sender: TObject);
     procedure FormCreate(Sender: TObject);
   private
 
@@ -21,6 +28,7 @@ type
 
 var
   Form1: TForm1;
+  bitmapR, bitmapG, bitmapB : array [0..2000, 0..2000] of byte;
 
 implementation
 
@@ -31,6 +39,25 @@ implementation
 procedure TForm1.FormCreate(Sender: TObject);
 begin
 
+end;
+
+procedure TForm1.loadButtonClick(Sender: TObject);
+var
+    x, y : integer;
+begin
+  if OpenPictureDialog1.Execute then
+    begin
+      Image1.Picture.LoadFromFile(OpenPictureDialog1.FileName);
+      for y:=0 to image1.Height-1 do
+      begin
+        for x:=0 to image1.Width-1 do
+        begin
+          bitmapR[x,y] := GetRValue(image1.Canvas.Pixels[x,y]);
+          bitmapG[x,y] := GetGValue(image1.Canvas.Pixels[x,y]);
+          bitmapB[x,y] := GetBValue(image1.Canvas.Pixels[x,y]);
+        end;
+      end;
+    end;
 end;
 
 end.
--- unit1.pas
+++ unit1.pas
@@ -5,7 +5,8 @@ unit Unit1;
 interface
 
 uses
-  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, ExtCtrls, StdCtrls;
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, ExtCtrls, StdCtrls,
+  ExtDlgs, windows;
 
 type
 
@@ -13,10 +14,13 @@ type
 
   TForm1 = class(TForm)
     loadButton: TButton;
+    OpenPictureDialog1: TOpenPictureDialog;
     saveButton: TButton;
     Image1: TImage;
+    SavePictureDialog1: TSavePictureDialog;
     procedure loadButtonClick(Sender: TObject);
     procedure FormCreate(Sender: TObject);
+    procedure saveButtonClick(Sender: TObject);
   private
 
   public
@@ -25,6 +29,7 @@ type
 
 var
   Form1: TForm1;
+  bitmapR, bitmapG, bitmapB : array [0..2000, 0..2000] of byte;
 
 implementation
 
@@ -37,9 +42,31 @@ begin
 
 end;
 
-procedure TForm1.loadButtonClick(Sender: TObject);
+procedure TForm1.saveButtonClick(Sender: TObject);
 begin
+  if SavePictureDialog1.Execute then
+  begin
+    image1.Picture.SaveToFile(SavePictureDialog1.FileName);
+  end;
+end;
 
+procedure TForm1.loadButtonClick(Sender: TObject);
+var
+    x, y : integer;
+begin
+  if OpenPictureDialog1.Execute then
+    begin
+      Image1.Picture.LoadFromFile(OpenPictureDialog1.FileName);
+      for y:=0 to image1.Height-1 do
+      begin
+        for x:=0 to image1.Width-1 do
+        begin
+          bitmapR[x,y] := GetRValue(image1.Canvas.Pixels[x,y]);
+          bitmapG[x,y] := GetGValue(image1.Canvas.Pixels[x,y]);
+          bitmapB[x,y] := GetBValue(image1.Canvas.Pixels[x,y]);
+        end;
+      end;
+    end;
 end;
 
 end.
