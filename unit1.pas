ca3a343c69a76a801f668658646262b12d3db9a6
willysusilo <58355441+willysusilo@users.noreply.github.com>
2020-10-01 10:37:40 +0700

    Refactored Aryo's code, Added reset image button
    Kalo ada yang bingung dari code ini, tanya gw aja

--- unit1.pas
+++ unit1.pas
@@ -14,6 +14,7 @@ type
 
   TForm1 = class(TForm)
     brightnessLabel: TLabel;
+    resetButton: TButton;
     ButtonInvers: TButton;
     ButtonSmooth: TButton;
     resetBrightnessButton: TButton;
@@ -30,6 +31,7 @@ type
     procedure ButtonSmoothClick(Sender: TObject);
     procedure loadButtonClick(Sender: TObject);
     procedure resetBrightnessButtonClick(Sender: TObject);
+    procedure resetButtonClick(Sender: TObject);
     procedure saveButtonClick(Sender: TObject);
     procedure TrackBarBrightnessChange(Sender: TObject);
   private
@@ -42,7 +44,7 @@ var
   Form1: TForm1;
   bitmapR, bitmapG, bitmapB : array [0..1000, 0..1000] of Byte; //variabel untuk gambar yang default belom dimanipulasi
   manipR, manipG, manipB : array [0..1000, 0..1000] of Byte; //variabel untuk gambar yang termanipulasi
-  defaultBrightnessR, defaultBrightnessG, defaultBrightnessB : array [0..2000, 0..2000] of Byte; //variabel untuk default brightness
+  defaultBrightnessR, defaultBrightnessG, defaultBrightnessB : array [0..1000, 0..1000] of Byte; //variabel untuk default brightness
 
 implementation
 
@@ -59,6 +61,29 @@ begin
       for x:=0 to image1.Width-1 do
       begin
         image1.Canvas.Pixels[x,y] := RGB(defaultBrightnessR[x,y], defaultBrightnessG[x,y], defaultBrightnessB[x,y]);
+        manipR[x,y] := defaultBrightnessR[x,y];
+        manipG[x,y] := defaultBrightnessG[x,y];
+        manipB[x,y] := defaultBrightnessB[x,y];
+      end;
+    end;
+  TrackBarBrightness.Position := 0;
+end;
+
+procedure TForm1.resetButtonClick(Sender: TObject);
+var
+  x,y : integer;
+begin
+  for y:=0 to image1.Height-1 do
+    begin
+      for x:=0 to image1.Width-1 do
+      begin
+        image1.Canvas.Pixels[x,y] := RGB(bitmapR[x,y], bitmapG[x,y], bitmapB[x,y]);
+        defaultBrightnessR[x,y] := bitmapR[x,y];
+        manipR[x,y] := bitmapR[x,y];
+        defaultBrightnessG[x,y] := bitmapG[x,y];
+        manipG[x,y] := bitmapG[x,y];
+        defaultBrightnessB[x,y] := bitmapB[x,y];
+        manipB[x,y] := bitmapB[x,y];
       end;
     end;
   TrackBarBrightness.Position := 0;
@@ -113,8 +138,9 @@ begin
           defaultBrightnessR[x,y] := bitmapR[x,y];
           defaultBrightnessG[x,y] := bitmapG[x,y];
           defaultBrightnessB[x,y] := bitmapB[x,y];
-
-
+          manipR[x,y] := bitmapR[x,y];
+          manipG[x,y] := bitmapG[x,y];
+          manipB[x,y] := bitmapB[x,y];
         end;
       end;
     end;
@@ -129,17 +155,23 @@ begin
   begin
    for x:= 0 to image1.width-1 do
    begin
-    gausR := (bitmapR[x-1][y-1] * 0.075) + (bitmapR[x][y-1] * 0.124) + (bitmapR[x+1][y-1] * 0.075)
-              + (bitmapR[x-1][y] * 0.075) + (bitmapR[x][y] * 0.124) + (bitmapR[x+1][y] * 0.075)
-              + (bitmapR[x-1][y+1] * 0.075) + (bitmapR[x][y+1] * 0.124) + (bitmapR[x+1][y+1] * 0.075);
-    gausG := (bitmapG[x-1][y-1] * 0.075) + (bitmapG[x][y-1] * 0.124) + (bitmapG[x+1][y-1] * 0.075)
-              + (bitmapG[x-1][y] * 0.075) + (bitmapG[x][y] * 0.124) + (bitmapG[x+1][y] * 0.075)
-              + (bitmapG[x-1][y+1] * 0.075) + (bitmapG[x][y+1] * 0.124) + (bitmapG[x+1][y+1] * 0.075);
-    gausB := (bitmapB[x-1][y-1] * 0.075) + (bitmapB[x][y-1] * 0.124) + (bitmapB[x+1][y-1] * 0.075)
-              + (bitmapB[x-1][y] * 0.075) + (bitmapB[x][y] * 0.124) + (bitmapB[x+1][y] * 0.075)
-              + (bitmapB[x-1][y+1] * 0.075) + (bitmapB[x][y+1] * 0.124) + (bitmapB[x+1][y+1] * 0.075);
+    gausR := (manipR[x-1][y-1] * 0.075) + (manipR[x][y-1] * 0.124) + (manipR[x+1][y-1] * 0.075)
+              + (manipR[x-1][y] * 0.075) + (manipR[x][y] * 0.124) + (manipR[x+1][y] * 0.075)
+              + (manipR[x-1][y+1] * 0.075) + (manipR[x][y+1] * 0.124) + (manipR[x+1][y+1] * 0.075);
+    gausG := (manipG[x-1][y-1] * 0.075) + (manipG[x][y-1] * 0.124) + (manipG[x+1][y-1] * 0.075)
+              + (manipG[x-1][y] * 0.075) + (manipG[x][y] * 0.124) + (manipG[x+1][y] * 0.075)
+              + (manipG[x-1][y+1] * 0.075) + (manipG[x][y+1] * 0.124) + (manipG[x+1][y+1] * 0.075);
+    gausB := (manipB[x-1][y-1] * 0.075) + (manipB[x][y-1] * 0.124) + (manipB[x+1][y-1] * 0.075)
+              + (manipB[x-1][y] * 0.075) + (manipB[x][y] * 0.124) + (manipB[x+1][y] * 0.075)
+              + (manipB[x-1][y+1] * 0.075) + (manipB[x][y+1] * 0.124) + (manipB[x+1][y+1] * 0.075);
 
     image1.Canvas.Pixels[x,y] := RGB(Trunc(gausR), Trunc(gausG), Trunc(gausB));
+    defaultBrightnessR[x,y] := Trunc(gausR/((100 + TrackBarBrightness.Position)/100));
+    defaultBrightnessG[x,y] := Trunc(gausG/((100 + TrackBarBrightness.Position)/100));
+    defaultBrightnessB[x,y] := Trunc(gausB/((100 + TrackBarBrightness.Position)/100));
+    manipR[x,y] := Trunc(gausR);
+    manipG[x,y] := Trunc(gausG);
+    manipB[x,y] := Trunc(gausB);
    end;
   end;
 
@@ -147,17 +179,20 @@ end;
 
 procedure TForm1.ButtonInversClick(Sender: TObject);
 var
-  x, y, ir, ig, ib : integer;
+  x, y: integer;
 begin
   for y:= 0 to image1.height-1 do
   begin
    for x:= 0 to image1.width-1 do
    begin
-    ir := 255 - bitmapR[x,y];
-    ig := 255 - bitmapG[x,y];
-    ib := 255 - bitmapB[x,y];
+    manipR[x,y] := 255 - manipR[x,y];
+    manipG[x,y] := 255 - manipG[x,y];
+    manipB[x,y] := 255 - manipB[x,y];
 
-    image1.Canvas.Pixels[x,y] := RGB(ir, ig, ib);
+    image1.Canvas.Pixels[x,y] := RGB(manipR[x,y], manipG[x,y], manipB[x,y]);
+    defaultBrightnessR[x,y] := Trunc(manipR[x,y]/((100 + TrackBarBrightness.Position)/100));
+    defaultBrightnessG[x,y] := Trunc(manipG[x,y]/((100 + TrackBarBrightness.Position)/100));
+    defaultBrightnessB[x,y] := Trunc(manipB[x,y]/((100 + TrackBarBrightness.Position)/100));
    end;
   end;
 
